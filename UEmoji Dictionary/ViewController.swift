5007c2ba74502e03b9ed10b7294e58380f06ae51
Bill Raynor <bill.raynor@new.rr.com>
2017-05-30 10:46:33 -0500

    Added initial tableView (UITableView) to Storyboard and to C ontroller

--- UEmoji Dictionary/ViewController.swift
+++ UEmoji Dictionary/ViewController.swift
@@ -8,11 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var EmojiTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        EmojiTableView.dataSource = self
+        EmojiTableView.delegate = self
+        
     }
 
     override func didReceiveMemoryWarning() {
