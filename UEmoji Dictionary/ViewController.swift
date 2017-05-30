a00146c80eb07934824aaee10407b7c6fded0b18
Bill Raynor <bill.raynor@new.rr.com>
2017-05-30 11:32:45 -0500

    added filler to tableView

--- UEmoji Dictionary/ViewController.swift
+++ UEmoji Dictionary/ViewController.swift
@@ -19,7 +19,16 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         EmojiTableView.delegate = self
         
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "🤠"
+        return cell
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
